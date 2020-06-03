d323bbad6a0713f4096a2d44385c2986d8f38d8f
Shubham Raj <ce19b030@iittp.ac.in>
2020-06-03 11:16:20 +0530

    Updates

---
+++ precache-manifest.8f7718a517190f3c920039dfc1932d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff2c8ea49e5e3938ff109a292f993bf",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "aca5ff3ec365f37d2e2a",
+    "url": "/To-Do-List/static/css/main.784447c4.chunk.css"
+  },
+  {
+    "revision": "2004dda3b6605746dd92",
+    "url": "/To-Do-List/static/js/2.59e73282.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.59e73282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca5ff3ec365f37d2e2a",
+    "url": "/To-Do-List/static/js/main.8ac3db87.chunk.js"
+  },
+  {
+    "revision": "7873e09fb1dfb746de72",
+    "url": "/To-Do-List/static/js/runtime-main.1ed45569.js"
+  }
+]);
